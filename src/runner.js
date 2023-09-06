f29def59eda078a3fbf06e1d5d424ca3207cab88
Tiago Loureiro <tiago.loureiro@jumpseller.com>
2023-09-06 09:56:08 +0100

    improv on output style

--- src/runner.js
+++ src/runner.js
@@ -7,26 +7,35 @@ export class Runner {
     this.proposal = proposal;
   }
 
+  // connects to external console
+  // runs repro-check script
+  // prints logs to the local console
+  // returns logs as an array of strings
   call() {
+    let logStream = [];
+
     // connect
     const conn = new Client();
     conn
       .on("ready", () => {
-        console.log("Client :: ready");
+        console.log("Console :: start");
+        console.log("Console :: run repro check");
         conn.exec("./repro-check.sh -p " + this.proposal, (err, stream) => {
           if (err) throw err;
           stream
             .on("close", (code, signal) => {
               console.log(
-                "Stream :: close :: code: " + code + ", signal: " + signal
+                "Console :: end :: code: " + code + ", signal: " + signal
               );
               conn.end();
             })
             .on("data", (data) => {
-              console.log("STDOUT: " + data);
+              console.log(data);
+              logStream.push(data);
             })
             .stderr.on("data", (data) => {
-              console.log("STDERR: " + data);
+              console.log(data);
+              logStream.push(data);
             });
         });
       })
@@ -36,5 +45,7 @@ export class Runner {
         username: "root",
         privateKey: readFileSync(SSH_KEY_PATH),
       });
+
+    return logStream;
   }
 }
