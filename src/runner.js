7f4789e281e736658fe98c7ddcc128a68f6c5490
tiago89 <19423709+tiagoicp@users.noreply.github.com>
2023-09-06 10:12:23 +0100

    Feat/improv output style (#2)
    * improv on output style
    
    * improv on output style
    
    ---------
    
    Co-authored-by: Tiago Loureiro <tiago.loureiro@jumpseller.com>

--- src/runner.js
+++ src/runner.js
@@ -7,26 +7,35 @@ export class Runner {
     this.proposal = proposal;
   }
 
+  // connects to external console
+  // runs repro-check script
+  // prints logs to the local console
+  // returns logs as an array of strings
   call() {
+    let logStream = [];
+
     // connect
     const conn = new Client();
     conn
       .on("ready", () => {
-        console.log("Client :: ready");
+        console.log("Console :: start");
+        console.log("Console :: run repro check");
         conn.exec("./repro-check.sh -p " + this.proposal, (err, stream) => {
           if (err) throw err;
           stream
             .on("close", (code, signal) => {
               console.log(
-                "Stream :: close :: code: " + code + ", signal: " + signal
+                "Console :: end :: code: " + code + ", signal: " + signal
               );
               conn.end();
             })
             .on("data", (data) => {
-              console.log("STDOUT: " + data);
+              console.log(this.cleanStream(data));
+              logStream.push(this.cleanStream(data));
             })
             .stderr.on("data", (data) => {
-              console.log("STDERR: " + data);
+              console.log(this.cleanStream(data));
+              logStream.push(this.cleanStream(data));
             });
         });
       })
@@ -36,5 +45,12 @@ export class Runner {
         username: "root",
         privateKey: readFileSync(SSH_KEY_PATH),
       });
+
+    return logStream;
+  }
+
+  cleanStream(dataStream) {
+    // trim() will remove the excessive break line at end of a string
+    return dataStream.toString().trim();
   }
 }
