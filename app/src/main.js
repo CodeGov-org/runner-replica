abb0cc1c676d84aef10ef3e3fa48813ca334d2c2
tiago89 <19423709+tiagoicp@users.noreply.github.com>
2023-09-26 21:53:56 +0100

    added store param (#6)
    * added store param
    
    * fixed secret location
    
    * removed unused entrypoint
    
    ---------
    
    Co-authored-by: Tiago Loureiro <tiago.loureiro@jumpseller.com>

--- app/src/main.js
+++ app/src/main.js
@@ -1,15 +1,33 @@
 #!/usr/bin/env node
 
 import { Runner } from "./runner.js";
+import { getSharedData } from "./config.js";
 
-// TODO: add DSCVR POST env
-// Required Env Var
-const proposal = process.env.PROPOSAL;
-if (!proposal) {
-  console.log("Missing required envs: PROPOSAL");
-  process.exit(1);
-}
+const main = async () => {
+  const proposal = await getNextProposal();
+  if (!proposal) {
+    console.log("No new proposals found.");
+    process.exit(1);
+  }
 
-// Main script
-const runner = new Runner(proposal);
-await runner.call();
+  const runner = new Runner(proposal);
+  const result = await runner.call();
+
+  return result;
+};
+
+const getNextProposal = async () => {
+  const sharedData = await getSharedData();
+  let nextProposal = "";
+  for (let entry of sharedData) {
+    if (entry.finished_at != "") continue;
+
+    nextProposal = entry.proposal;
+    break;
+  }
+
+  return nextProposal;
+};
+
+// Script is called here
+await main();
--- docker-entrypoint.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/bash
-
-# check argument passed to docker run command
-PROPOSAL=$1 DSCVR_POST=$2 npx runner
