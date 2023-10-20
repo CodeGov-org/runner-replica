cd34febb32760087f940966b101c6375b8d4ce26
Tiago Loureiro <tiago.loureiro@jumpseller.com>
2023-10-20 11:00:51 +0200

    added topic and adopted flag and improved error handling

--- lambda/checkProposalsAndStartTask/index.mjs
+++ lambda/checkProposalsAndStartTask/index.mjs
@@ -1,13 +1,24 @@
 import https from "https";
 
 export const handler = async (event) => {
-  const proposals = await get_proposals();
+  const proposals = await get_open_replica_proposals();
+  if (proposals.data.length == 0) {
+    console.log("Success: No Open Proposals Found");
+    return { statusCode: 200, body: "No open proposals found" };
+  }
+
+  // parse to get proposal ids
+  // update store prop
+  // execute task (if open)
 
   return { statusCode: 200, body: JSON.stringify(proposals) };
 };
 
-async function get_proposals() {
-  const url = "https://ic-api.internetcomputer.org/api/v3/proposals?limit=100";
+handler();
+
+async function get_open_replica_proposals() {
+  const url =
+    "https://ic-api.internetcomputer.org/api/v3/proposals?include_topic=TOPIC_REPLICA_VERSION_MANAGEMENT&include_status=OPEN";
 
   return new Promise((resolve) => {
     https
@@ -19,14 +30,21 @@ async function get_proposals() {
           data += chunk;
         });
 
-        // The whole response has been received. Print out the result.
+        // The whole response has been received. Handle the result.
         resp.on("end", () => {
-          resolve(data);
+          const result = JSON.parse(data);
+          // if success
+          if ("data" in result) {
+            resolve(result);
+          } else {
+            console.log("Error, API Proposals Failed Response: " + data);
+            process.exit(1);
+          }
         });
       })
       .on("error", (err) => {
-        console.log("Error: " + err.message);
-        resolve(false);
+        console.log("Error on API Proposals: " + err.message);
+        process.exit(1);
       });
   });
 }
